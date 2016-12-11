edb91d5c3c7bdb7733099c06995022e0ead2480f
earthgecko <gary.wilson@of-networks.co.uk>
2016-12-11 13:23:07 +0000

    ionosphere.py doc Ionosphere v1.1.0-alpha IssueID #922: Ionosphere IssueID #1658: Pattering Skyline Ionosphere
    - Adding the meat to Ionosphere bones on the service side, incomplete.
    
    Modified:
    skyline/ionosphere/ionosphere.py

--- skyline/ionosphere/ionosphere.py
+++ skyline/ionosphere/ionosphere.py
@@ -11,6 +11,9 @@ except:
 from time import time, sleep
 from threading import Thread
 from multiprocessing import Process, Manager
+import re
+from shutil import rmtree
+import csv
 
 from redis import StrictRedis
 from msgpack import Unpacker, packb
@@ -18,8 +21,14 @@ import traceback
 import mysql.connector
 from mysql.connector import errorcode
 
+from sqlalchemy.sql import select
+import requests
+from requests.auth import HTTPBasicAuth
+
 import settings
 from skyline_functions import load_metric_vars, fail_check, mysql_select, write_data_to_file, mkdir_p
+
+from database import get_engine, ionosphere_table_meta, metrics_table_meta
 
 skyline_app = 'ionosphere'
 skyline_app_logger = '%sLog' % skyline_app
@@ -104,8 +113,8 @@ class Ionosphere(Thread):
         """
         Insert data into mysql table
 
-        :param select: the insert string
-        :type select: str
+        :param insert: the insert string
+        :type insert: str
         :return: int
         :rtype: int or boolean
 
@@ -153,25 +162,31 @@ class Ionosphere(Thread):
 
     def purge_old_data_dirs(self, dir_path, older_than):
         time_now = time()
-        logger.info('checking for old data to remove')
-        for path, folders, files in os.walk(dir_path):
-            for folder in folders[:]:
-                folder_path = os.path.join(path, folder)
-                # if (time_now - os.path.getmtime(folder_path)) > older_than:
-                #     yield folder_path
-                for i_file in files:
-                    current_file = '%s/%s' % (str(folder_path), str(i_file))
-                    if (time_now - os.path.getmtime(current_file)) > older_than:
-                        yield current_file
-                        os.remove(current_file)
-                        logger.info('removing old data - %s' % str(current_file))
-                        if len(os.listdir(folder_path)) == 0:
-                            logger.info('removing old data dir - %s' % str(folder_path))
-                            os.rmdir(folder_path)
+        logger.info(
+            'Cleaning old training data from %s older than %s seconds' %
+            (dir_path, str(older_than)))
+        try:
+            for path, folders, files in os.walk(dir_path):
+                for folder in folders[:]:
+                    folder_path = os.path.join(path, folder)
+                    # Only timestamped directories are removed
+                    if re.match('\d{10}', folder):
+                        if ENABLE_IONOSPHERE_DEBUG:
+                            logger.info('debug :: matched - %s' % folder_path)
+                        if (time_now - os.path.getmtime(folder_path)) > older_than:
+                            try:
+                                rmtree(folder_path)
+                                logger.info('removed - %s' % folder_path)
+                            except:
+                                logger.error('error :: failed to rmtree %s' % folder_path)
+        except:
+            logger.info(traceback.format_exc())
+            logger.error('error :: purge_old_data_dirs - os.walk')
+
         last_purge_ts = int(time())
         try:
             self.redis_conn.setex(last_purge_key, 1800, last_purge_ts)
-            logger.info('updated Redis key for %s up' % last_purge_key)
+            logger.info('updated Redis key for %s' % last_purge_key)
         except:
             logger.error('error :: failed to update Redis key for %s' % last_purge_key)
 
@@ -181,6 +196,15 @@ class Ionosphere(Thread):
             logger.info('updated the backup_purge_ts_file with %s' % str(last_purge_ts))
         except:
             logger.error('error :: failed to update the backup_purge_ts_file - %s' % backup_purge_ts_file)
+        return
+
+    def remove_metric_check_file(self, metric_check_file):
+        if os.path.isfile(str(metric_check_file)):
+            try:
+                os.remove(str(metric_check_file))
+                logger.info('metric_check_file removed - %s' % str(metric_check_file))
+            except OSError:
+                pass
         return
 
     def spin_process(self, i, metric_check_file):
@@ -244,6 +268,7 @@ class Ionosphere(Thread):
         try:
             metric_vars.metric
             metric = str(metric_vars.metric)
+            base_name = metric
             if settings.ENABLE_IONOSPHERE_DEBUG:
                 logger.info('debug :: metric variable - metric - %s' % metric)
         except:
@@ -330,28 +355,193 @@ class Ionosphere(Thread):
             logger.info(
                 'Ionosphere check max age exceeded - %s - %s seconds old, older than %s seconds discarding' % (
                     metric, str(anomaly_age), str(max_age_seconds)))
-            if os.path.isfile(str(metric_check_file)):
-                try:
-                    os.remove(str(metric_check_file))
-                    logger.info('metric_check_file removed - %s' % str(metric_check_file))
-                except OSError:
-                    pass
+            self.remove_metric_check_file(str(metric_check_file))
             return
 
-        # Match the check to a Panorama anomaly
+        # Check if the metric has ionosphere_enabled, if not remove the check
+        # file but not the data directory
         query = "SELECT ionosphere_enabled FROM metrics WHERE metric='%s'" % metric
         result = mysql_select(skyline_app, query)
         if str(result[0]) != '1':
             logger.info('Ionosphere not enabled on %s' % (metric))
-            if os.path.isfile(str(metric_check_file)):
-                try:
-                    os.remove(str(metric_check_file))
-                    logger.info('metric_check_file removed - %s' % str(metric_check_file))
-                except OSError:
-                    pass
+            self.remove_metric_check_file(str(metric_check_file))
+            return
+        else:
+            logger.info('Ionosphere is enabled on %s' % (metric))
+
+        # @added 20161210 - Branch #922: ionosphere
+        #                   Task #1658: Patterning Skyline Ionosphere
+        # Only continue if there is a training data json timeseries file
+        metric_timeseries_dir = base_name.replace('.', '/')
+        metric_training_data_dir = '%s/%s/%s' % (
+            settings.IONOSPHERE_DATA_FOLDER, metric_timestamp,
+            metric_timeseries_dir)
+        anomaly_json = '%s/%s.json' % (metric_training_data_dir, base_name)
+        if os.path.isfile(anomaly_json):
+            logger.info('training data ts json available - %s' % (anomaly_json))
+        else:
+            logger.error('error :: training data ts json was not found - %s' % (anomaly_json))
+            self.remove_metric_check_file(str(metric_check_file))
             return
 
-        return False
+        # @added 20161209 - Branch #922: ionosphere
+        #                   Task #1658: Patterning Skyline Ionosphere
+        # Use SQLAlchemy, mysql.connector is still upstairs ^^ but starting the
+        # move to SQLAlchemy now that all the webapp Ionosphere SQLAlchemy
+        # patterns work and the database lay out if defined we can begin on the
+        # data side.  Ionosphere was put together backwards, like tsfresh was
+        # learnt.  It was the people input first here in many ways, which is
+        # exactly how it was suppose to be.
+        # This is now the Ionosphere meat.
+        # Get a MySQL engine
+        logger.info('getting MySQL engine')
+        try:
+            engine, fail_msg, trace = get_an_engine()
+            logger.info(fail_msg)
+        except:
+            trace = traceback.format_exc()
+            logger.error(trace)
+            fail_msg = 'error :: could not get a MySQL engine'
+            logger.error('%s' % fail_msg)
+            self.remove_metric_check_file(str(metric_check_file))
+            return False
+
+        if not engine:
+            trace = 'none'
+            fail_msg = 'error :: engine not obtained'
+            logger.error(fail_msg)
+            return False
+
+        # Get the ionosphere_table metadata
+        ionosphere_table = None
+        try:
+            ionosphere_table, fail_msg, trace = ionosphere_table_meta(skyline_app, engine)
+            logger.info(fail_msg)
+        except:
+            trace = traceback.format_exc()
+            logger.error('%s' % trace)
+            fail_msg = 'error :: failed to get ionosphere_table meta for %s' % base_name
+            logger.error('%s' % fail_msg)
+            return False
+
+        logger.info('ionosphere_table OK')
+
+        # Determine the fp_ids that exist for the metric
+        fp_ids = []
+        fp_ids_found = False
+        try:
+            connection = engine.connect()
+            stmt = select([ionosphere_table.c.fp_id]).where(ionosphere_table.c.metric == base_name)
+            result = connection.execute(stmt)
+            for row in result:
+                fp_id = row['id']
+                fp_ids.append(int(fp_id))
+            connection.close()
+            fp_count = len(fp_ids)
+            logger.info('determined %s fp ids for %s' % (str(fp_count), base_name))
+            return False
+        except:
+            logger.error(traceback.format_exc())
+            logger.error('error :: could not determine fp ids from DB for %s' % base_name)
+
+        if fp_ids == []:
+            logger.error('error :: there are no fp ids for %s' % base_name)
+        else:
+            fp_ids_found = True
+
+        # @added 20161210 - Branch #922: ionosphere
+        #                   Task #1658: Patterning Skyline Ionosphere
+        # Calculate features for the current timeseries is there are fp ids
+        # Just call it via the webapp... fewer lines of code and already done in
+        # webapp/ionosphere_backend.py and webapp/features_proifle.py
+        webapp_url = '%s/ionosphere?timestamp=%s&metric=%s&calc_features=true' % (
+            settings.SKYLINE_URL, timestamp, base_name)
+        r = None
+        http_status_code = 0
+        if settings.WEBAPP_AUTH_ENABLED:
+            timeout_and_auth = 'timeout=10, auth=(%s, %s))' % (settings.WEBAPP_AUTH_USER, settings.WEBAPP_AUTH_USER_PASSWORD)
+        else:
+            timeout_and_auth = 'timeout=10'
+        if fp_ids_found:
+            for _ in range(2):
+                try:
+                    r = requests.get(webapp_url, timeout_and_auth)
+                    http_status_code = r.status_code
+                except:
+                    logger.error('error :: could not retrieve %s' % webapp_url)
+                    sleep(1)
+                    continue
+                else:
+                    break
+            else:
+                logger.error(traceback.format_exc())
+                logger.error('error :: could not retrieve %s after 3 tries' % webapp_url)
+
+        # Check the features were calculated by the webapp
+        calculated_feature_file = '%s/%s.tsfresh.input.csv.features.transposed.csv' % (metric_training_data_dir, base_name)
+        calculated_feature_file_found = False
+        if int(http_status_code) == 200:
+            if os.path.isfile(calculated_feature_file):
+                logger.info('calculated features available - %s' % (calculated_feature_file))
+                calculated_feature_file_found = True
+            else:
+                logger.error('error :: calculated features not available - %s' % (calculated_feature_file))
+                self.remove_metric_check_file(str(metric_check_file))
+                return
+
+        # Create an array of the calculated features
+        calculated_features = []
+        if calculated_feature_file_found:
+            count_id = 0
+            with open(calculated_feature_file, 'rb') as fr:
+                reader = csv.reader(fr, delimiter=',')
+                for i, line in enumerate(reader):
+                    if str(line[0]) != '':
+                        if ',' in line[0]:
+                            feature_name = '"%s"' % str(line[0])
+                        else:
+                            feature_name = str(line[0])
+                        count_id += 1
+                        value = float(line[1])
+                        calculated_features.append([feature_name, value])
+
+        if calculated_features == []:
+            logger.error('error :: no calculated features were determined from - %s' % (calculated_feature_file))
+            self.remove_metric_check_file(str(metric_check_file))
+            return
+
+        # Compare calculated features to feature values for each fp id
+        fp_ids_found = False
+        not_anomalous = False
+        if calculated_feature_file_found:
+            for fp_ip in fp_ids:
+                # Get features for fp_id from z_fp_<metric_id> table.
+                # Determine what features each data has, extract only values for
+                # common features.
+                # Determine the sum of each set
+                # Determine whether each set is positive or negative
+                # # if the same carry on
+                # # if both negative, make then both positive
+                # Sum fp values, Sum calculated - handle negatives like features_sum :: -3389570699080000.0000000000
+                # Determine whether each set is positive or negative
+                # # if the same carry on
+                # # if both negative, make then both positive postive_sums
+                # if diff_in_sums <= 1%:
+                #     log
+                #     update matched_count in ionosphere_table
+                #     not_anomalous = True
+                logger.debug('debug :: %s is feature profile for %s' % (str(fp_id), base_name))
+                fp_ids_found = True
+
+            # if not not_anomalous:
+            #     log anomaly
+            #     send to panorama
+            #     send anomalous count metric to graphite
+            #     alert
+
+        # TO BE REMOVED
+        self.remove_metric_check_file(str(metric_check_file))
+        return fp_ids_found
 
     def run(self):
         """
@@ -389,15 +579,6 @@ class Ionosphere(Thread):
         else:
             logger.info('bin/%s.d log management done' % skyline_app)
 
-        # See if I am known in the DB, if so, what are my variables
-        # self.populate mysql
-        # What is my host id in the Skyline panorama DB?
-        #   - if not known - INSERT hostname INTO hosts
-        # What are the known apps?
-        #   - if returned make a dictionary
-        # What are the known algorithms?
-        #   - if returned make a dictionary
-
         while 1:
             now = time()
 
@@ -420,36 +601,21 @@ class Ionosphere(Thread):
             except:
                 logger.error('error :: failed to update Redis key for %s up' % skyline_app)
 
-            if ENABLE_IONOSPHERE_DEBUG:
-                # Make sure mysql is available
-                mysql_down = True
-                while mysql_down:
-
-                    query = 'SHOW TABLES'
-                    results = mysql_select(skyline_app, query)
-
-                    if results:
-                        mysql_down = False
-                        logger.info('debug :: tested database query - OK')
-                    else:
-                        logger.error('error :: failed to query database')
-                        sleep(30)
-
-            if ENABLE_IONOSPHERE_DEBUG:
-                try:
-                    query = 'SELECT id, test FROM test'
-                    result = mysql_select(skyline_app, query)
-                    logger.info('debug :: tested mysql SELECT query - OK')
-                    logger.info('debug :: result: %s' % str(result))
-                    logger.info('debug :: result[0]: %s' % str(result[0]))
-                    logger.info('debug :: result[1]: %s' % str(result[1]))
-# Works
-# 2016-06-10 19:07:23 :: 4707 :: result: [(1, u'test1')]
-                except:
-                    logger.error(
-                        'error :: mysql error - %s' %
-                        traceback.print_exc())
-                    logger.error('error :: failed to SELECT')
+            # purge_old_data_dirs after every check file run, this takes less
+            # than a second and keeps the purging somewhat consistent with
+            # input rate.
+            try:
+                logger.info('purging any old training data')
+                self.purge_old_data_dirs(
+                    settings.IONOSPHERE_DATA_FOLDER,
+                    settings.IONOSPHERE_KEEP_TRAINING_TIMESERIES_FOR)
+            except:
+                logger.error('error :: purge_old_data_dirs - %s' % traceback.print_exc())
+                if ENABLE_IONOSPHERE_DEBUG:
+                    logger.info(
+                        'debug :: self.purge_old_data_dirs(%s, %s)' %
+                        settings.IONOSPHERE_DATA_FOLDER,
+                        settings.IONOSPHERE_KEEP_TRAINING_TIMESERIES_FOR)
 
             # self.populate the database metatdata tables
             # What is my host id in the Skyline panorama DB?
@@ -462,7 +628,7 @@ class Ionosphere(Thread):
             else:
                 logger.info('failed to determine host id of %s' % this_host)
 
-            #   - if not known - INSERT hostname INTO host
+            # if not known - INSERT hostname INTO host
             if not host_id:
                 logger.info('inserting %s into hosts table' % this_host)
                 query = 'insert into hosts (host) VALUES (\'%s\')' % this_host
@@ -476,21 +642,6 @@ class Ionosphere(Thread):
                     this_host)
                 sleep(30)
                 continue
-
-            # Like loop through the panorama dir and see if anyone has left you
-            # any work, etc
-            # Make sure check_dir exists and has not been removed
-            try:
-                if settings.ENABLE_IONOSPHERE_DEBUG:
-                    logger.info('debug :: checking check dir exists - %s' % settings.IONOSPHERE_CHECK_PATH)
-                os.path.exists(settings.IONOSPHERE_CHECK_PATH)
-            except:
-                logger.error('error :: check dir did not exist - %s' % settings.IONOSPHERE_CHECK_PATH)
-                mkdir_p(settings.IONOSPHERE_CHECK_PATH)
-
-                logger.info('check dir created - %s' % settings.IONOSPHERE_CHECK_PATH)
-                os.path.exists(settings.IONOSPHERE_CHECK_PATH)
-                # continue
 
             """
             Determine if any metric has been added to add
@@ -528,11 +679,6 @@ class Ionosphere(Thread):
             metric_check_file = '%s/%s' % (settings.IONOSPHERE_CHECK_PATH, str(metric_var_files_sorted[0]))
 
             logger.info('processing - %s' % str(metric_var_files_sorted[0]))
-
-            # TODO
-            # Spawn a process to purge_old_data_dirs every 30 minutes
-            # Actually thinking about a cron to do this, it is well suited, even
-            # if it is a cron to call ionosphere_purge_old_data.py
 
             # Spawn processes
             pids = []
