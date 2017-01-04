b1fd994566f11bcd16fa6c69f02e31e6bd0f17e7
earthgecko <gary.wilson@of-networks.co.uk>
2017-01-04 19:15:53 +0000

    Panorama check file fails #24 - v1.1.0-alpha IssueID #1830: Ionosphere alerts IssueID #1460: panorama check file fails
    - Removed the buggy skyline_functions.load_metric_vars and replaced with the
      new_load_metric_vars(self, metric_vars_file) function
    - Fixes #14
    - Changed ionosphere_backend.py to the new function
    
    Modified:
    skyline/panorama/panorama.py
    skyline/webapp/ionosphere_backend.py

--- skyline/webapp/ionosphere_backend.py
+++ skyline/webapp/ionosphere_backend.py
@@ -213,6 +213,90 @@ def ionosphere_metric_data(requested_timestamp, data_for_metric, context):
     """
     Get a list of all training data folders and metrics
     """
+
+    # @added 20170104 - Feature #1842: Ionosphere - Graphite now graphs
+    #                   Feature #1830: Ionosphere alerts
+    # Use the new_load_metric_vars method
+    def new_load_metric_vars(metric_vars_file):
+        """
+        Load the metric variables for a check from a metric check variables file
+
+        :param metric_vars_file: the path and filename to the metric variables files
+        :type metric_vars_file: str
+        :return: the metric_vars module object or ``False``
+        :rtype: list
+
+        """
+        if path.isfile(metric_vars_file):
+            logger.info(
+                'loading metric variables from metric_check_file - %s' % (
+                    str(metric_vars_file)))
+        else:
+            logger.error(
+                'error :: loading metric variables from metric_check_file - file not found - %s' % (
+                    str(metric_vars_file)))
+            return False
+
+        metric_vars = []
+        with open(metric_vars_file) as f:
+            for line in f:
+                no_new_line = line.replace('\n', '')
+                no_equal_line = no_new_line.replace(' = ', ',')
+                array = str(no_equal_line.split(',', 1))
+                add_line = literal_eval(array)
+                metric_vars.append(add_line)
+
+        string_keys = ['metric', 'anomaly_dir', 'added_by', 'app', 'source']
+        float_keys = ['value']
+        int_keys = ['from_timestamp', 'metric_timestamp', 'added_at', 'full_duration']
+        array_keys = ['algorithms', 'triggered_algorithms']
+        boolean_keys = ['graphite_metric', 'run_crucible_tests']
+
+        metric_vars_array = []
+        for var_array in metric_vars:
+            key = None
+            value = None
+            if var_array[0] in string_keys:
+                key = var_array[0]
+                value_str = str(var_array[1]).replace("'", '')
+                value = str(value_str)
+                if var_array[0] == 'metric':
+                    metric = value
+            if var_array[0] in float_keys:
+                key = var_array[0]
+                value_str = str(var_array[1]).replace("'", '')
+                value = float(value_str)
+            if var_array[0] in int_keys:
+                key = var_array[0]
+                value_str = str(var_array[1]).replace("'", '')
+                value = int(value_str)
+            if var_array[0] in array_keys:
+                key = var_array[0]
+                value = literal_eval(str(var_array[1]))
+            if var_array[0] in boolean_keys:
+                key = var_array[0]
+                if str(var_array[1]) == 'True':
+                    value = True
+                else:
+                    value = False
+            if key:
+                metric_vars_array.append([key, value])
+
+            if len(metric_vars_array) == 0:
+                logger.error(
+                    'error :: loading metric variables - none found' % (
+                        str(metric_vars_file)))
+                return False
+
+            if settings.ENABLE_DEBUG:
+                logger.info(
+                    'debug :: metric_vars determined - metric variable - metric - %s' % str(metric_vars.metric))
+
+        logger.info('debug :: metric_vars for %s' % str(metric))
+        logger.info('debug :: %s' % str(metric_vars_array))
+
+        return metric_vars_array
+
     base_name = data_for_metric.replace(settings.FULL_NAMESPACE, '', 1)
     if context == 'training_data':
         log_context = 'training data'
@@ -251,15 +335,20 @@ def ionosphere_metric_data(requested_timestamp, data_for_metric, context):
     metric_vars = ['error: could not read metrics vars file', metric_vars_file]
     if path.isfile(metric_vars_file):
         try:
-            metric_vars = []
-            with open(metric_vars_file) as f:
-                for line in f:
-                    add_line = line.replace('\n', '')
-                    metric_vars.append(add_line)
-
+            # @modified 20170104 - Feature #1842: Ionosphere - Graphite now graphs
+            #                      Feature #1830: Ionosphere alerts
+            # Use the new_load_metric_vars method
+            # metric_vars = []
+            # with open(metric_vars_file) as f:
+            #     for line in f:
+            #         add_line = line.replace('\n', '')
+            #         metric_vars.append(add_line)
+            metric_vars = new_load_metric_vars(metric_vars_file)
             metric_vars_ok = True
         except:
             metric_vars_ok = False
+            logger.error(traceback.format_exc())
+            logger.error('error :: failed to load metric_vars from: %s' % str(metric_vars_file))
 
     ts_json_ok = False
     ts_json = ['error: could not timeseries json file', ts_json_file]
