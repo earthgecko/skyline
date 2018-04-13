f1166cbbdc49c2f6b8e15dbd2e4c0507e858ea00
earthgecko <gary.wilson@of-networks.co.uk>
2018-04-13 14:49:13 +0100

    correct luminosity SQL

--- updates/sql/ionosphere-v1.1.x-to-v1.2.0.sql
+++ updates/sql/ionosphere-v1.1.x-to-v1.2.0.sql
@@ -4,12 +4,13 @@ This the the SQL script to update Skyline from ionosphere (v1.1.2 - v1.1.3)
 
 USE skyline;
 
-/*
-# @added 20170402 - Feature #2000: Ionosphere - validated
-#                   Branch #922: ionosphere
-*/
-ALTER TABLE `ionosphere` ADD COLUMN `validated` INT(10) DEFAULT 0 COMMENT 'unix timestamp when validated, 0 being none' AFTER `generation`;
-COMMIT;
-ALTER TABLE `ionosphere_layers` ADD COLUMN `validated` INT(10) DEFAULT 0 COMMENT 'unix timestamp when validated, 0 being none' AFTER `label`;
-COMMIT;
-UPDATE ionosphere SET validated=1 WHERE generation < 2;
+# @added 20180413 - Branch #2270: luminosity
+CREATE TABLE IF NOT EXISTS `luminosity` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'anomaly id',
+  `metric_id` INT(11) NOT NULL COMMENT 'metric id',
+  `coefficient` DECIMAL(6,5) NOT NULL COMMENT 'correlation coefficient',
+  `shifted` TINYINT NOT NULL COMMENT 'shifted',
+  `shifted_coefficient` DECIMAL(6,5) NOT NULL COMMENT 'shifted correlation coefficient',
+  PRIMARY KEY (id),
+  INDEX `luminosity` (`id`,`metric_id`))
+  ENGINE=InnoDB;
