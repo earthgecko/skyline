13a7c84eb32e4056c7901662faccbd2fe7d21ad5
Dvir Mizrahi <dvirm@wix.com>
2019-04-09 01:53:42 +0300

    added initial infra for docker build

---
+++ init.sh
@@ -0,0 +1,21 @@
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/2.7.14/Python-2.7.14.tgz
+sudo tar xzf Python-2.7.14.tgz
+cd Python-2.7.14
+sudo ./configure --enable-optimizations
+sudo make install
+sudo rm -rf /usr/src/Python-2.7.14*
+
+python -V
+
+mkdir -p /var/log/skyline
+mkdir -p /var/run/skyline
+mkdir -p /var/dump
+mkdir -p /opt/skyline/panorama/check
+mkdir -p /opt/skyline/mirage/check
+mkdir -p /opt/skyline/crucible/check
+mkdir -p /opt/skyline/crucible/data
+mkdir -p /opt/skyline/ionosphere
+mkdir -p /etc/skyline
+mkdir -p /tmp/skyline
+
