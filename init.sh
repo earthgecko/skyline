63610e1989fbc60e2689e904b80eb0e1dcbecbce
Dvir Mizrahi <dvirm@wix.com>
2019-04-10 15:16:37 +0300

    chaning unicode to ucs4

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ cd /usr/src
 sudo wget https://www.python.org/ftp/python/2.7.14/Python-2.7.14.tgz
 sudo tar xzf Python-2.7.14.tgz
 cd Python-2.7.14
-sudo ./configure --enable-optimizations
+sudo ./configure --enable-optimizations --enable-unicode=ucs4 && make
 sudo make install
 sudo rm -rf /usr/src/Python-2.7.14*
 
