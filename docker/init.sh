16ad1b694c6c369bd0521f8cc898669f2f7f54a1
Dvir Mizrahi <dvirm@wix.com>
2019-04-11 00:16:28 +0300

    separated base image after installation from skyline image prone to changes

---
+++ docker/init.sh
@@ -0,0 +1,15 @@
+mkdir -p /var/log/skyline
+mkdir -p /var/run/skyline
+mkdir -p /var/dump
+mkdir -p /opt/skyline/panorama/check
+mkdir -p /opt/skyline/mirage/check
+mkdir -p /opt/skyline/crucible/check
+mkdir -p /opt/skyline/crucible/data
+mkdir -p /opt/skyline/ionosphere
+mkdir -p /etc/skyline
+mkdir -p /tmp/skyline
+
+
+cp /skyline/etc/skyline_docker.conf /etc/skyline/skyline.conf
+
+
--- docker/install_environment.sh
+++ docker/install_environment.sh
@@ -8,29 +8,10 @@ sudo rm -rf /usr/src/Python-2.7.14*
 
 python -V
 
-mkdir -p /var/log/skyline
-mkdir -p /var/run/skyline
-mkdir -p /var/dump
-mkdir -p /opt/skyline/panorama/check
-mkdir -p /opt/skyline/mirage/check
-mkdir -p /opt/skyline/crucible/check
-mkdir -p /opt/skyline/crucible/data
-mkdir -p /opt/skyline/ionosphere
-mkdir -p /etc/skyline
-mkdir -p /tmp/skyline
-
 cd /skyline
-
 sudo pip install docutils
 sudo pip install $(cat requirements.txt | grep "^numpy\|^scipy\|^patsy" | tr '\n' ' ')
 sudo pip install $(cat requirements.txt | grep "^pandas")
 sudo pip install -r requirements.txt
+sudo pip install --upgrade setuptools
 
-
-cp /skyline/etc/skyline_docker.conf /etc/skyline/skyline.conf
-
-bin/horizon.d start
-bin/analyzer.d start
-bin/webapp.d start
-
-tail -f /var/log/skyline/*
