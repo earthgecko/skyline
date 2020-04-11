27834a80e70769daf7194af706f0d0287d93a793
earthgecko <gary.wilson@of-networks.co.uk>
2020-04-11 16:29:32 +0100

    sql_versions IssueID #3478: sql_versions table
    - Added as patch-3478
    
    Added:
    updates/sql/v2.0.0-patch-3478.sql
    Modified:
    skyline/skyline.sql
    updates/sql/v2.0.0.sql

---
+++ updates/sql/v2.0.0-patch-3478.sql
@@ -0,0 +1,17 @@
+/*
+This is the SQL script to update Skyline from v2.0.0 with SQL patch-3478
+*/
+
+USE skyline;
+
+/*
+# @added 20200411 - Feature #3478: sql_versions table
+#                   Branch #3262: py3
+# Added a versions table to the DB as a method to track what version of the DB schema is being run.
+# This eases tracking and skipping versions by know what DB updates need to be applied.
+*/
+CREATE TABLE IF NOT EXISTS `sql_versions` (
+  `version` VARCHAR(255) DEFAULT NULL COMMENT 'version',
+  `created_timestamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'created timestamp')
+  ENGINE=InnoDB;
+INSERT INTO `sql_versions` (version) VALUES ('2.0.0');
