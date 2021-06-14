e799074b402a24c3713fd40972b47956b2c192dd
earthgecko <gary.wilson@of-networks.co.uk>
2021-06-14 17:43:44 +0100

    Merge luminosity.PR429 PR #429 IssueID #4102: Merge luminosity.PR429
    Modified:
    skyline/luminosity/luminosity.py

--- skyline/luminosity/luminosity.py
+++ skyline/luminosity/luminosity.py
@@ -697,7 +697,12 @@ class Luminosity(Thread):
                     # Check MySQL
                     now = int(time())
                     after = now - 600
-                    query = 'SELECT * FROM anomalies WHERE anomaly_timestamp > \'%s\'' % str(after)  # nosec
+                    # @modified 20210525 - https://github.com/earthgecko/skyline/pull/429
+                    #                      Task #4102: Merge luminosity.PR429
+                    # More efficient query, although it is only getting a few rows from the DB
+                    # every little helps (thanks @ashemez)
+                    # query = 'SELECT * FROM anomalies WHERE anomaly_timestamp > \'%s\'' % str(after)  # nosec
+                    query = 'SELECT id FROM anomalies WHERE anomaly_timestamp > \'%s\'' % str(after)  # nosec
                     results = None
                     try:
                         results = mysql_select(skyline_app, query)
@@ -718,7 +723,12 @@ class Luminosity(Thread):
                 if str(last_processed_anomaly_id) == 'None':
                     last_processed_anomaly_id = 0
 
-                query = 'SELECT * FROM anomalies WHERE id > \'%s\'' % str(last_processed_anomaly_id)  # nosec
+			    # @modified 20210525 - https://github.com/earthgecko/skyline/pull/429
+			    #                      Task #4102: Merge luminosity.PR429
+                # More efficient query, although it is only getting a few rows from the DB
+       			# every little helps (thanks @ashemez)
+                # query = 'SELECT * FROM anomalies WHERE id > \'%s\'' % str(last_processed_anomaly_id)  # nosec
+                query = 'SELECT id FROM anomalies WHERE id > \'%s\'' % str(last_processed_anomaly_id)  # nosec
                 results = None
                 try:
                     results = mysql_select(skyline_app, query)
