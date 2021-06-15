1f544c001c6c403238a7f050e065a0d32566a616
earthgecko <gary.wilson@of-networks.co.uk>
2021-06-15 12:11:48 +0100

    Ionosphere - inference
    IssueID #4014: Ionosphere - inference
    IssueID #3590: inference
    
    - Added updates/sql/v2.1.0-patch-dev-4014.sql SQL to skyline.sql
    
    Modified:
    skyline/skyline.sql

--- skyline/skyline.sql
+++ skyline/skyline.sql
@@ -274,7 +274,13 @@ CREATE TABLE IF NOT EXISTS `ionosphere` (
   `matched_count` INT(11) DEFAULT 0 COMMENT 'the number of times this feature profile has been matched',
   `last_matched` INT(10) DEFAULT 0 COMMENT 'the unix timestamp of the last time this feature profile (or other) was matched',
   `last_checked` INT(10) DEFAULT 0 COMMENT 'the unix timestamp of the last time this feature profile was checked',
+/*
+# @added 20210414 - Feature #4014: Ionosphere - inference
+#                   Branch #3590: inference
+# Removed used column which was replace when the checked_count column was added
+# below under #1830 and #2546, so check_count was there but never used.
   `check_count` INT(10) DEFAULT 0 COMMENT 'the number of times this feature profile has been checked',
+*/
   `created_timestamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'created timestamp',
 /*
 # @added 20161229 - Feature #1830: Ionosphere alerts
@@ -289,6 +295,15 @@ CREATE TABLE IF NOT EXISTS `ionosphere` (
 */
   `parent_id` INT(10) DEFAULT 0 COMMENT 'the id of the parent features profile, 0 being the original human generated features profile',
   `generation` INT DEFAULT 0 COMMENT 'the number of generations between this feature profile and the original, 0 being the original human generated features profile',
+/*
+# @added 20210414 - Feature #4014: Ionosphere - inference
+#                   Branch #3590: inference
+# Added motif related columns
+*/
+  `motif_matched_count` INT(11) DEFAULT 0 COMMENT 'the number of times a motif from this feature profile has been matched',
+  `motif_last_matched` INT(10) DEFAULT 0 COMMENT 'the unix timestamp of the last time a motif from this feature profile was matched',
+  `motif_last_checked` INT(10) DEFAULT 0 COMMENT 'the unix timestamp of the last time a motif from this feature profile was checked',
+  `motif_checked_count` INT(10) DEFAULT 0 COMMENT 'the number of times a motifs from this feature profile have been checked',
 /*
 # @added 20170402 - Feature #2000: Ionosphere - validated
 */
@@ -395,6 +410,11 @@ Added fp_count and fp_checked
 */
   `fp_count` INT(10) DEFAULT 0 COMMENT 'the total number of features profiles for the metric which were valid to check',
   `fp_checked` INT(10) DEFAULT 0 COMMENT 'the number of features profiles checked until the match was made',
+/*
+# @added 20210412 - Feature #4014: Ionosphere - inference
+#                   Branch #3590: inference
+*/
+  `motifs_matched_id` INT DEFAULT NULL COMMENT 'the motif match id from the motifs_matched table, if this was a motif matched',
 /*
 # @added 20190601 - Feature #3084: Ionosphere - validated matches
 */
@@ -523,21 +543,6 @@ INSERT INTO `users` (user,description) VALUES ('Skyline','The default Skyline us
 INSERT INTO `users` (user,description) VALUES ('admin','The default admin user');
 
 /*
-# @added 20200411 - Feature #3478: sql_versions table
-#                   Branch #3262: py3
-# Added a versions table to the DB as a method to track what version of the DB schema is being run.
-# This eases tracking and skipping versions by know what DB updates need to be applied.
-# patch-3478
-*/
-CREATE TABLE IF NOT EXISTS `sql_versions` (
-  `version` VARCHAR(255) DEFAULT NULL COMMENT 'version',
-  `created_timestamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'created timestamp')
-  ENGINE=InnoDB;
-/* INSERT INTO `sql_versions` (version) VALUES ('2.0.0'); */
-/* INSERT INTO `sql_versions` (version) VALUES ('2.1.0'); */
-INSERT INTO `sql_versions` (version) VALUES ('2.1.0-patch-dev-3978-3642');
-
-/*
 # @added 20200928 - Task #3748: POC SNAB
 #                   Branch #3068: SNAB
 */
@@ -589,6 +594,78 @@ CREATE TABLE IF NOT EXISTS `anomalies_type` (
   `metric_id` INT(11) NOT NULL COMMENT 'metric id',
   `type` VARCHAR(255) NOT NULL COMMENT 'a csv list of the anomaly_types ids e.g 1,2,3',
   INDEX `anomalies_type_id` (`id`,`metric_id`)) ENGINE=InnoDB;
+
+/*
+# @added 20210412 - Feature #4014: Ionosphere - inference
+#                   Branch #3590: inference
+*/
+CREATE TABLE IF NOT EXISTS `motif_matched_types` (
+  `id` INT(11) NOT NULL COMMENT 'matched type id',
+  `type` VARCHAR(255) NOT NULL COMMENT 'motif match type name, e.g. exact`',
+  PRIMARY KEY (id),
+  INDEX `motif_matched_types` (`id`,`type`)) ENGINE=InnoDB;
+/* NOTICE: these types are also maintained in ionosphere/inference.py */
+INSERT INTO `motif_matched_types` (`id`,`type`) VALUES (1, 'exact');
+INSERT INTO `motif_matched_types` (`id`,`type`) VALUES (2, 'all_in_range');
+INSERT INTO `motif_matched_types` (`id`,`type`) VALUES (3, 'in_range');
+INSERT INTO `motif_matched_types` (`id`,`type`) VALUES (4, 'not_similar_enough');
+INSERT INTO `motif_matched_types` (`id`,`type`) VALUES (5, 'INVALIDATED');
+INSERT INTO `motif_matched_types` (`id`,`type`) VALUES (6, 'distance');
+
+CREATE TABLE IF NOT EXISTS `motifs_matched` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'inference matched unique id',
+  `metric_id` INT(11) NOT NULL COMMENT 'metric id',
+  `fp_id` INT(11) NOT NULL COMMENT 'features profile id',
+  `metric_timestamp` INT(10) DEFAULT 0 COMMENT 'the unix timestamp of the not anomalous datapoint that matched the motif of the fp',
+  `primary_match` TINYINT(4) DEFAULT 0 COMMENT 'whether the match is the primary match for the instance',
+  `index` INT(11) NOT NULL COMMENT 'the index of the motif within the features profile timeseries',
+  `size` INT(11) NOT NULL COMMENT 'the size of the motif',
+  `distance` DOUBLE DEFAULT 0 COMMENT 'the motif distance',
+/* @added 202210427 - motif_area, fp_motif_area and area_percent_diff */
+  `motif_area` DOUBLE DEFAULT 0 COMMENT 'the motif area from the composite trapezoidal rule',
+  `fp_motif_area` DOUBLE DEFAULT 0 COMMENT 'the fp motif area from the composite trapezoidal rule',
+  `area_percent_diff` DOUBLE DEFAULT 0 COMMENT 'the percent difference of the motif_area from the fp_motif_area',
+/* @added 202210428 - fps_checked and runtime */
+  `fps_checked` INT(11) DEFAULT 0 COMMENT 'the number of features profiles checked',
+  `runtime` DOUBLE DEFAULT 0 COMMENT 'the inference runtime',
+  `type_id` INT(11) DEFAULT 0 COMMENT 'the match type id',
+  `validated` TINYINT(4) DEFAULT 0 COMMENT 'whether the match has been validated, 0 being no, 1 validated, 2 invalid',
+  `user_id` INT DEFAULT NULL COMMENT 'the user id that validated the match',
+  PRIMARY KEY (id),
+  INDEX `inference_motifs_matched` (`id`,`metric_id`,`fp_id`,`metric_timestamp`,`primary_match`,`validated`))
+  ENGINE=InnoDB;
+/*
+# @added 20210414 - Feature #4014: Ionosphere - inference
+#                   Branch #3590: inference
+# Store the not anomalous motifs
+*/
+CREATE TABLE IF NOT EXISTS `not_anomalous_motifs` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'motif element unique id',
+  `motif_id` INT(11) NOT NULL COMMENT 'motif id',
+  `timestamp` INT(10) DEFAULT 0 COMMENT 'motif element unix timestamp',
+  `value` DECIMAL(65,6) NOT NULL COMMENT 'motif element value',
+  PRIMARY KEY (id),
+  INDEX `inference_not_anomalous_motifs` (`id`,`motif_id`))
+  ENGINE=InnoDB;
+
+
+/*
+# @added 20200411 - Feature #3478: sql_versions table
+#                   Branch #3262: py3
+# Added a versions table to the DB as a method to track what version of the DB schema is being run.
+# This eases tracking and skipping versions by know what DB updates need to be applied.
+# patch-3478
+*/
+CREATE TABLE IF NOT EXISTS `sql_versions` (
+  `version` VARCHAR(255) DEFAULT NULL COMMENT 'version',
+  `created_timestamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'created timestamp')
+  ENGINE=InnoDB;
+/* INSERT INTO `sql_versions` (version) VALUES ('2.0.0'); */
+/* INSERT INTO `sql_versions` (version) VALUES ('2.1.0'); */
+/* INSERT INTO `sql_versions` (version) VALUES ('2.1.0-patch-dev-3978-3642'); */
+/* INSERT INTO `sql_versions` (version) VALUES ('2.1.0-patch-dev-4014'); */
+
+INSERT INTO `sql_versions` (version) VALUES ('2.1.0');
 
 /*
 # mariadb
