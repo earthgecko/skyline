a3878b2f3d514f199f8526f894931752166af16d
earthgecko <gary.wilson@of-networks.co.uk>
2026-06-15 19:54:01 +0100

    Update for v5.0.0 IssueID #5627: v5.0.0 update dependencies IssueID #5713: Test CentOS Stream 10
    - metric_name_labels_parser.py changes for v5.0.0
    
    Modified:
    skyline/functions/prometheus/metric_name_labels_parser.py

--- skyline/functions/prometheus/metric_name_labels_parser.py
+++ skyline/functions/prometheus/metric_name_labels_parser.py
@@ -3,7 +3,12 @@ Parse a Prometheus metric and determine metric name and labels
 """
 import logging
 
-from prometheus_client.parser import _parse_labels as parse_labels
+# @modified 20250617 - Task #5627: v5.0.0 update dependencies
+# _parse_labels changed to parse_labels in v0.22.0
+try:
+    from prometheus_client.parser import _parse_labels as parse_labels
+except:
+    from prometheus_client.parser import parse_labels
 
 
 # @added 20220616 - Branch #4300: prometheus
@@ -36,6 +41,17 @@ def metric_name_labels_parser(current_skyline_app, metric):
         current_logger = logging.getLogger(current_skyline_app_logger)
         current_logger.error('error :: %s :: failed to parse metric %s - %s' % (
             function_str, str(metric), str(err)))
+        return metric_dict
+
+    # @added 20260427 - Task #5713: Test CentOS Stream 10
+    # parse_labels hangs if the labels do not end in ", occurs in while loop
+    # and something is obviously not handled promptly.
+    # https://github.com/prometheus/client_python/blob/482656c8c07b78668adb5f3171dfe71ccc2396b2/prometheus_client/parser.py#L61
+    if not metric_labels.endswith('"'):
+        current_skyline_app_logger = current_skyline_app + 'Log'
+        current_logger = logging.getLogger(current_skyline_app_logger)
+        current_logger.error('error :: %s :: invalid metric_labels to parse, does not end with a ", metric_labels: %s, metric: %s' % (
+            function_str, str(metric_labels), str(metric)))
         return metric_dict
 
     labels = {}
