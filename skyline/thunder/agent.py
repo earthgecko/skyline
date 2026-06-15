68551264da0eddb9b49fea964d5836dce8b3493f
earthgecko <gary.wilson@of-networks.co.uk>
2026-06-15 20:24:38 +0100

    Update for v5.0.0 IssueID #5627: v5.0.0 update dependencies
    - agent.py changes for v5.0.0
    
    Modified:
    skyline/thunder/agent.py

--- skyline/thunder/agent.py
+++ skyline/thunder/agent.py
@@ -2,7 +2,11 @@ import logging
 import sys
 from os import getpid
 from os.path import isdir
-from daemon import runner
+# @modified 20250612 - Support #4860: python-daemon-3.0.0 - runner deprecated
+#                      Task #5627: v5.0.0 update dependencies
+#from daemon import runner
+import daemon
+
 from time import sleep
 import os.path
 
@@ -22,6 +26,9 @@ if True:
     from validate_settings import validate_settings_variables
     # @added 20220328 - Feature #4018: thunder - skyline.errors
     from functions.redis.RedisErrorLogHandler import RedisErrorLogHandler
+    # @added 20250612 - Support #4860: python-daemon-3.0.0 - runner deprecated
+    #                   Task #5627: v5.0.0 update dependencies
+    import service_runner as runner
 
 skyline_app = 'thunder'
 skyline_app_logger = '%sLog' % skyline_app
